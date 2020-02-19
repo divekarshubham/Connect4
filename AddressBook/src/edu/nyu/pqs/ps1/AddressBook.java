b561bdcb900200cac809b4b694577cf5b01287dd
Shubham Divekar <divekarshubham@gmail.com>
2020-02-18 22:58:54 -0500

    Changing directory structure

---
+++ AddressBook/src/edu/nyu/pqs/ps1/AddressBook.java
@@ -0,0 +1,317 @@
+package edu.nyu.pqs.ps1;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * AddressBook class represents the collection of contacts stored in a TreeSet
+ * and the operation performed to add, modify and delete contacts. Contacts can
+ * be added using a Contact object, List of Contacts and from the file that
+ * stores contacts in a specific JSON format.
+ *
+ * You can initialize an empty AddressBook with the parameterless constructor
+ * and the contents are stored to a new file (AddressBook-Date.json) or from a
+ * stored AddressBook with the filename. All changes made to the AddressBook are
+ * then saved to the same file(using the saveState() function.
+ *
+ * Duplicates are automatically eliminated out of the box with the use of
+ * TreeSet and the use of Comparator in the Contacts class
+ *
+ * Class also overrides equals(), hashCode(), and toString() for better
+ * readability and usability
+ *
+ * @author Shubham Divekar (sjd451)
+ */
+public class AddressBook {
+
+    /** Storing all the contacts as a TreeSet to maintain order */
+    private Set<Contact> storage;
+
+    /** Location to store/read the addressbook from */
+    private String filePath;
+
+    public AddressBook() {
+        storage = new TreeSet<>();
+    }
+
+    public AddressBook(String filePath) {
+        storage = new TreeSet<>(importFromFile(filePath));
+        this.filePath = filePath;
+    }
+
+    /**
+     * If filePath is not specified create one of the format AddressBook-[Date].json
+     */
+    public void saveState() {
+        if (filePath == null || filePath.equals("")) {
+            String filePath = "AddressBook-" + LocalDate.now() + ".json";
+            exportToFile(filePath);
+            this.filePath = filePath;
+        } else
+            exportToFile(this.filePath);
+    }
+
+    /**
+     * Use the simple-json library to create JSONObjects and serialize the
+     * addressbook and write it to the specified path.
+     *
+     * @param filePath Path to write the storage
+     */
+    @SuppressWarnings("unchecked")
+    private void exportToFile(String filePath) {
+        /** Create an array of all the contacts and populate it with the contacts in the storage */
+        JSONArray addressBook = new JSONArray();
+        for (Contact contact : storage) {
+            JSONObject contactDetails = new JSONObject();
+            contactDetails.put("Name", contact.getName());
+            contactDetails.put("PhoneNumber", contact.getPhone());
+            contactDetails.put("Address", contact.getAddress());
+            contactDetails.put("E-Mail", contact.getEmail());
+            contactDetails.put("Note", contact.getNote());
+
+            JSONObject contactObject = new JSONObject();
+            contactObject.put("Contact", contactDetails);
+            addressBook.add(contactObject);
+        }
+
+        try (FileWriter file = new FileWriter(filePath)) {
+            file.write(addressBook.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Read the entire dictionary and parse individual contacts to get the details
+     * and then add them to the list. This may throw a FileNotFoundException if
+     * filePath is incorrect or an IOException or a ParseException is the JSON is
+     * not in the desired format.
+     *
+     * @param filePath file where the dictionary is stored
+     * @return List of contacts from the JSON file
+     */
+    @SuppressWarnings("unchecked")
+    private List<Contact> importFromFile(String filePath) {
+        JSONParser jsonParser = new JSONParser();
+        List<Contact> contacts = new ArrayList<>();
+        try (FileReader reader = new FileReader(filePath)) {
+            JSONArray addressBook = (JSONArray) jsonParser.parse(reader);
+            addressBook.forEach(c -> contacts.add(parseContacts((JSONObject) c)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contacts;
+    }
+
+    /**
+     * Parse each Contact fields from the JSON input file and extract the details
+     * into an Contact
+     *
+     * @param contact Contact derived from the JSONObject array
+     * @return Contact object from the builder
+     */
+    private Contact parseContacts(JSONObject contact) {
+        JSONObject contactObject = (JSONObject) contact.get("Contact");
+        return new Contact.Builder((String) contactObject.get("Name"))
+            .phoneNumber((String) contactObject.get("Phone"))
+            .address((String) contactObject.get("Address"))
+            .email((String) contactObject.get("E-Mail"))
+            .note((String) contactObject.get("Note")).build();
+    }
+
+    /**
+     * Add individual contacts from Contact objects and they can be chained
+     *
+     * @param contact
+     * @return AddressBook reference
+     */
+    public AddressBook addContact(Contact contact) {
+        storage.add(contact);
+        return this;
+    }
+
+    /**
+     * Add multiple contacts from a List of Contact objects and they can be chained
+     *
+     * @param contacts
+     * @return AddressBook reference
+     */
+    public AddressBook addMultipleContacts(List<Contact> contacts) {
+        storage.addAll(contacts);
+        return this;
+    }
+
+    /**
+     * Add multiple contacts from a a new file and they can be chained
+     *
+     * @param filePath
+     * @return AddressBook reference
+     */
+    public AddressBook addContactsFromFile(String filePath) {
+        List<Contact> contacts = importFromFile(filePath);
+        storage.addAll(contacts);
+        return this;
+    }
+
+    /**
+     * Remove contacts that satisfy a particular query in the specified field
+     *
+     * @param parameter The field you want to search in
+     * @param query     Remove a contact that contains the following query
+     * @return Number of contacts removed
+     */
+    public int removeContactByField(ContactFields parameter, String query) {
+        List<Contact> contacts = searchByField(parameter, query);
+        storage.removeAll(contacts);
+        return contacts.size();
+    }
+
+    /**
+     * Remove multiple contacts from the AddressBook
+     *
+     * @param contacts List of contacts to be removed
+     * @return Number of contacts removed
+     */
+    public int removeMultipleContacts(List<Contact> contacts) {
+        if (storage.removeAll(contacts))
+            return contacts.size();
+        return 0;
+    }
+
+    /**
+     * Remove a single contact from the AddressBook
+     *
+     * @param contact Contact to be removed
+     * @return 1 if contact found and removed else 0
+     */
+    public int removeContact(Contact contact) {
+        if (storage.contains(contact)) {
+            storage.remove(contact);
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Universal search returns a contact if the given query is found as a substring
+     * within any of fields of a contact. The toString() of the contact is used to
+     * generate the summary and search
+     *
+     * @param query Substring to search
+     * @return List of contacts containing the substring
+     */
+    public List<Contact> search(String query) {
+        query = query.toLowerCase();
+        List<Contact> searchResults = new ArrayList<>();
+        for (Contact contact : storage) {
+            if (contact.toString().toLowerCase().contains(query))
+                searchResults.add(contact);
+        }
+        return searchResults;
+    }
+
+    /**
+     * Search a specific field of every contact for the subquery.
+     *
+     * @param parameter The ContactFields enum represents the parameter to search
+     *                  on.
+     * @param query     Substring to search
+     * @return List of contacts containing the substring
+     */
+    public List<Contact> searchByField(ContactFields parameter, String query) {
+        query = query.toLowerCase();
+        List<Contact> searchResults = new ArrayList<>();
+        for (Contact contact : storage) {
+            String field = "";
+            switch (parameter) {
+                case NAME:
+                    field = contact.getName().toLowerCase();
+                    break;
+                case PHONE:
+                    field = contact.getPhone().toLowerCase();
+                    break;
+                case ADDR:
+                    field = contact.getAddress().toLowerCase();
+                    break;
+                case EMAIL:
+                    field = contact.getEmail().toLowerCase();
+                    break;
+                case NOTE:
+                    field = contact.getNote().toLowerCase();
+                    break;
+            }
+            if (field.contains(query))
+                searchResults.add(contact);
+        }
+        return searchResults;
+    }
+
+    /**
+     * This uses the modify method of the contact to generate a clone with new
+     * parameter as specified by the user. If the specified contact is not present
+     * in the AddressBook an IllegalArgumentException is thrown else the contact is
+     * replaced with the modified version.
+     *
+     * @param contact Contact to modify
+     * @param field The ContactFields enum represents the parameter to modify
+     * @param newVal Changed value
+     */
+    public void modifyContact(Contact contact, ContactFields field, String newVal) {
+        if (storage.contains(contact)) {
+            storage.remove(contact);
+            contact = contact.modify(field, newVal);
+            storage.add(contact);
+        } else
+            throw new IllegalArgumentException("Contact not present in AddressBook");
+    }
+
+    /**
+     * Get a list of all the contacts in the AddressBook for viewing
+     *
+     * @return List of all Contacts
+     */
+    public List<Contact> getContacts() {
+        return new ArrayList<>(storage);
+    }
+
+    @Override
+    public String toString() {
+        return storage.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof AddressBook))
+            return false;
+        AddressBook addressBook = (AddressBook) o;
+        return getContacts().equals(addressBook.getContacts());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for (Contact contact : storage) {
+            hash += hash * 31 + contact.hashCode();
+        }
+        return hash;
+    }
+
+}
---
+++ AddressBook/src/edu/nyu/pqs/ps1/Contact.java
@@ -0,0 +1,241 @@
+package edu.nyu.pqs.ps1;
+import java.util.Comparator;
+
+/**
+ * The contact class stores individual entries of the address book. It consists
+ * of the details of a contact which are Name (mandatory), phone-number,
+ * address, email-address and a note. The class is constructed using the Builder
+ * pattern and the object created is immutable. A class is instantiated with a
+ * name and you can add additional details with the use of functions like
+ * .email(), .phone() etc.
+ *
+ * The class overrides the equals() and hashCode() functions to use the class
+ * with generics. toString() is implemented to make the contact information
+ * readable. The class also creates a COMPARATOR to make it usable by SortedSets
+ * and similar Collections.
+ *
+ * The modify method constructs and returns a new Object with one field changed
+ * to preserve immutability
+ *
+ * @author Shubham Divekar (sjd451)
+ */
+public class Contact implements Comparable<Contact> {
+    private final String name;
+    private final String address;
+    private final String phoneNumber;
+    private final String note;
+    private final String emailAddress;
+    /** Create a string lazily to be returned when toString is invoked */
+    private String contactInfo = "";
+
+    private Contact(Builder builder) {
+        this.name = builder.name;
+        this.address = builder.address;
+        this.phoneNumber = builder.phoneNumber;
+        this.note = builder.note;
+        this.emailAddress = builder.emailAddress;
+    }
+
+    /** Generic functions to get the details of the contact */
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone() {
+        return isNull(phoneNumber) ? "" : this.phoneNumber;
+    }
+
+    public String getAddress() {
+        return isNull(address) ? "" : this.address;
+    }
+
+    public String getEmail() {
+        return isNull(emailAddress) ? "" : this.emailAddress;
+    }
+
+    public String getNote() {
+        return isNull(note) ? "" : this.note;
+    }
+
+    private boolean isNull(Object s) {
+        return s == null;
+    }
+
+    /**
+     * Since the Contact is immutable we create a new Object and return it with the
+     * changed field. This method is package-private and can only be called by using
+     * the modifyContact() method in the AddressBook class
+     *
+     * @param parameter The field to be changed
+     * @param newVal    The value to be changed to
+     * @return New Contact with changed parameter
+     */
+    Contact modify(ContactFields parameter, String newVal) {
+        return new Builder(parameter == ContactFields.NAME ? newVal : this.name)
+            .phoneNumber(parameter == ContactFields.PHONE ? newVal : this.phoneNumber)
+            .address(parameter == ContactFields.ADDR ? newVal : this.address)
+            .email(parameter == ContactFields.EMAIL ? newVal : this.emailAddress)
+            .note(parameter == ContactFields.NOTE ? newVal : this.note).build();
+    }
+
+    /**
+     * Returns the comparison between two contacts and compares all the field
+     * values. For equivalency of contacts '+()-' characters are ignored from
+     * phone-numbers.
+     *
+     * @param o Contact to compare
+     * @return true if two contacts are equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Contact))
+            return false;
+        Contact contact = (Contact) o;
+        if (contact.name.equals(name)) {
+            if (contact.getPhone().replaceAll("[^\\d]", "")
+                .equals(this.getPhone().replaceAll("[^\\d]", ""))) {
+                if (contact.getAddress().equals(this.getAddress())) {
+                    if (contact.getEmail().equals(this.getEmail())) {
+                        if (contact.getNote().equals(this.getNote())) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * Generates hashCode based on all the parameters
+     *
+     * @return hashCode of the contact
+     */
+    @Override
+    public int hashCode() {
+        int hash = name.hashCode();
+        hash += isNull(phoneNumber) ? 0 : hash * 31 + phoneNumber.hashCode();
+        hash += isNull(address) ? 0 : hash * 31 + address.hashCode();
+        hash += isNull(emailAddress) ? 0 : hash * 31 + emailAddress.hashCode();
+        hash += isNull(note) ? 0 : hash * 31 + note.hashCode();
+        return hash;
+    }
+
+    /**
+     * Creates a string of contact information based on all available(not null)
+     * fields. It follows lazy initialization and created the string the first time
+     * the toString() is called since the class is immutable
+     *
+     * @return Contact information
+     */
+    @Override
+    public String toString() {
+        if (contactInfo.equals("")) {
+            StringBuilder contactInfo = new StringBuilder("Contact Info = { Name:" + this.name);
+            contactInfo.append(isNull(phoneNumber) ? "" : ", Phone:" + this.phoneNumber);
+            contactInfo.append(isNull(address) ? "" : ", Address:" + this.address);
+            contactInfo.append(isNull(emailAddress) ? "" : ", Email:" + this.emailAddress);
+            contactInfo.append(isNull(note) ? "" : ", Note:" + this.note);
+            contactInfo.append(" }");
+            this.contactInfo = contactInfo.toString();
+        }
+
+        return contactInfo;
+    }
+
+    /**
+     * Initialize a comparator to make the class be used efficiently with generics
+     * and Collections
+     */
+    private static final Comparator<Contact> COMPARATOR = Comparator.comparing((Contact c) -> c.name)
+        .thenComparing(c -> (c.phoneNumber == null) ? "" : c.phoneNumber.replaceAll("[^\\d]", ""))
+        .thenComparing(c -> (c.address == null) ? "" : c.address)
+        .thenComparing(c -> (c.emailAddress == null) ? "" : c.emailAddress)
+        .thenComparing(c -> (c.note == null) ? "" : c.note);
+
+    public int compareTo(Contact c) {
+        return COMPARATOR.compare(this, c);
+    }
+
+    /**
+     * Builder subclass to implement the Builder Pattern, wherein the name is passed
+     * to the Builder constructor and further parameters can be added using the
+     * email(), address() etc. functions, where the null or blank values are omitted
+     * from the object. All validations are performed inside the builder functions
+     * and throw IllegalArgumentException with different messages according to the
+     * validation and the field
+     */
+    public static class Builder {
+        private final String name;
+        private String address;
+        private String phoneNumber;
+        private String note;
+        private String emailAddress;
+
+        public Builder(String name) {
+            if (name.length() > 100)
+                throw new IllegalArgumentException("Name " + name + " cannot be greater than 100 characters");
+            if (name.length() == 0)
+                throw new IllegalArgumentException("Name cannot be blank");
+            this.name = name;
+        }
+
+        public Builder address(String val) {
+            if (val != null && !val.equals(""))
+                this.address = val;
+            return this;
+        }
+
+        public Builder phoneNumber(String val) {
+            /*
+             * Use Regular expression to evaluate the phone number (\+[0-9][0-9]?)? matches
+             * international codes +1 or +91 \d{10} matches 1234567890 (?:\d{3}-){2}\d{4}
+             * matches 123-456-7890 \(\d{3}\)\d{3}-?\d{4} matches (123)456-7890 or
+             * (123)4567890
+             */
+            if (val != null && !val.equals("")) {
+                String regex = "(\\+[0-9][0-9]?)?(\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4})";
+                if (!val.matches(regex)) {
+                    throw new IllegalArgumentException("Phone number " + val + " is invalid");
+                }
+                this.phoneNumber = val;
+            }
+            return this;
+        }
+
+        public Builder note(String val) {
+            if (val != null && !val.equals(""))
+                this.note = val;
+            return this;
+        }
+
+        public Builder email(String val) {
+            /*
+             * Use Regular expression to evaluate the email [\w-_.+]*[\w-_.]@1 or more
+             * characters with -_and . followed by @ ([\w]+\.)+ matches 1 or more domains
+             * like cs.nyu. or pqs.cs.nyu. [\w]+[\w] ends with an org like edu or com
+             */
+            if (val != null && !val.equals("")) {
+                String regex = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$";
+                if (!val.matches(regex)) {
+                    throw new IllegalArgumentException("Email " + val + " is invalid");
+                }
+                this.emailAddress = val;
+            }
+            return this;
+        }
+
+        /**
+         * Build and return the Contact by passing in the builder object created from
+         * the methods above
+         *
+         * @return Contact object
+         */
+        public Contact build() {
+            return new Contact(this);
+        }
+    }
+}
