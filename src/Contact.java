dc73faaef3fe231320d35830ac4520b71a4473ad
Shubham Divekar <divekarshubham@gmail.com>
2020-02-18 22:06:23 -0500

    Final documentation of the code

--- src/Contact.java
+++ src/Contact.java
@@ -1,30 +1,30 @@
+import java.util.Comparator;
+
 /**
- *  The contact class stores individual entries of the address book. It consists of the details of a
- *  contact which are Name (mandatory), phone-number, address, email-address and a note. The class
- *  is constructed using the Builder pattern and the object created is immutable. A class is
- *  instantiated with a name and you can add additional details with the use of functions like
- *  .email(), .phone() etc.
+ * The contact class stores individual entries of the address book. It consists
+ * of the details of a contact which are Name (mandatory), phone-number,
+ * address, email-address and a note. The class is constructed using the Builder
+ * pattern and the object created is immutable. A class is instantiated with a
+ * name and you can add additional details with the use of functions like
+ * .email(), .phone() etc.
  *
- * The class overrides the equals() and hashCode() functions to use the class with generics.
- * toString() is implemented to make the contact information readable. The class also creates a
- * COMPARATOR to make it usable by SortedSets and similar Collections.
+ * The class overrides the equals() and hashCode() functions to use the class
+ * with generics. toString() is implemented to make the contact information
+ * readable. The class also creates a COMPARATOR to make it usable by SortedSets
+ * and similar Collections.
  *
- * The modify method constructs and returns a new Object with one field changed to preserve
- * immutability
+ * The modify method constructs and returns a new Object with one field changed
+ * to preserve immutability
  *
  * @author Shubham Divekar (sjd451)
  */
-
-import java.util.Comparator;
-
-//Assumption: cannot add a contact without a name
 public class Contact implements Comparable<Contact> {
     private final String name;
     private final String address;
     private final String phoneNumber;
     private final String note;
     private final String emailAddress;
-    /** Create a string lazily to be returned when toString is invoked*/
+    /** Create a string lazily to be returned when toString is invoked */
     private String contactInfo = "";
 
     private Contact(Builder builder) {
@@ -35,7 +35,7 @@ public class Contact implements Comparable<Contact> {
         this.emailAddress = builder.emailAddress;
     }
 
-    /** Generic functions to get the details of the contact*/
+    /** Generic functions to get the details of the contact */
     public String getName() {
         return this.name;
     }
@@ -61,12 +61,12 @@ public class Contact implements Comparable<Contact> {
     }
 
     /**
-     * Since the Contact is immutable we create a new Object and return it with the changed field.
-     * This method is package-private and can only be called by using the modifyContact() method
-     * in the AddressBook class
+     * Since the Contact is immutable we create a new Object and return it with the
+     * changed field. This method is package-private and can only be called by using
+     * the modifyContact() method in the AddressBook class
      *
      * @param parameter The field to be changed
-     * @param newVal The value to be changed to
+     * @param newVal    The value to be changed to
      * @return New Contact with changed parameter
      */
     Contact modify(ContactFields parameter, String newVal) {
@@ -78,8 +78,9 @@ public class Contact implements Comparable<Contact> {
     }
 
     /**
-     * Returns the comparison between two contacts and compares all the field values. For
-     * equivalency of contacts '+()-' characters are ignored from phone-numbers.
+     * Returns the comparison between two contacts and compares all the field
+     * values. For equivalency of contacts '+()-' characters are ignored from
+     * phone-numbers.
      *
      * @param o Contact to compare
      * @return true if two contacts are equal
@@ -92,8 +93,8 @@ public class Contact implements Comparable<Contact> {
             return false;
         Contact contact = (Contact) o;
         if (contact.name.equals(name)) {
-            if (contact.getPhone().replaceAll("[^\\d]", "").equals(this.getPhone()
-                .replaceAll("[^\\d]", ""))) {
+            if (contact.getPhone().replaceAll("[^\\d]", "")
+                .equals(this.getPhone().replaceAll("[^\\d]", ""))) {
                 if (contact.getAddress().equals(this.getAddress())) {
                     if (contact.getEmail().equals(this.getEmail())) {
                         if (contact.getNote().equals(this.getNote())) {
@@ -123,9 +124,9 @@ public class Contact implements Comparable<Contact> {
     }
 
     /**
-     * Creates a string of contact information based on all available(not null) fields. It follows
-     * lazy initialization and created the string the first time the toString() is called since the
-     * class is immutable
+     * Creates a string of contact information based on all available(not null)
+     * fields. It follows lazy initialization and created the string the first time
+     * the toString() is called since the class is immutable
      *
      * @return Contact information
      */
@@ -145,7 +146,8 @@ public class Contact implements Comparable<Contact> {
     }
 
     /**
-     *  Initialize a comparator to make the class be used efficiently with generics and Collections
+     * Initialize a comparator to make the class be used efficiently with generics
+     * and Collections
      */
     private static final Comparator<Contact> COMPARATOR = Comparator.comparing((Contact c) -> c.name)
         .thenComparing(c -> (c.phoneNumber == null) ? "" : c.phoneNumber.replaceAll("[^\\d]", ""))
@@ -158,11 +160,12 @@ public class Contact implements Comparable<Contact> {
     }
 
     /**
-     * Builder subclass to implement the Builder Pattern, wherein the name is passed to the Builder
-     * constructor and further parameters can be added using the email(), address() etc. functions,
-     * where the null or blank values are omitted from the object.
-     * All validations are performed inside the builder functions and throw IllegalArgumentException
-     * with different messages according to the validation and the field
+     * Builder subclass to implement the Builder Pattern, wherein the name is passed
+     * to the Builder constructor and further parameters can be added using the
+     * email(), address() etc. functions, where the null or blank values are omitted
+     * from the object. All validations are performed inside the builder functions
+     * and throw IllegalArgumentException with different messages according to the
+     * validation and the field
      */
     public static class Builder {
         private final String name;
@@ -186,12 +189,11 @@ public class Contact implements Comparable<Contact> {
         }
 
         public Builder phoneNumber(String val) {
-            /**
-             * Use Regular expression to evaluate the phone number
-             * (\+[0-9][0-9]?)? matches international codes +1 or +91
-             * \d{10} matches 1234567890
-             * (?:\d{3}-){2}\d{4} matches 123-456-7890
-             * \(\d{3}\)\d{3}-?\d{4} matches (123)456-7890 or (123)4567890
+            /*
+             * Use Regular expression to evaluate the phone number (\+[0-9][0-9]?)? matches
+             * international codes +1 or +91 \d{10} matches 1234567890 (?:\d{3}-){2}\d{4}
+             * matches 123-456-7890 \(\d{3}\)\d{3}-?\d{4} matches (123)456-7890 or
+             * (123)4567890
              */
             if (val != null && !val.equals("")) {
                 String regex = "(\\+[0-9][0-9]?)?(\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4})";
@@ -210,11 +212,10 @@ public class Contact implements Comparable<Contact> {
         }
 
         public Builder email(String val) {
-            /**
-             * Use Regular expression to evaluate the email
-             * [\w-_.+]*[\w-_.]@1 or more characters with -_and . followed by @
-             * ([\w]+\.)+ matches 1 or more domains like cs.nyu. or pqs.cs.nyu.
-             * [\w]+[\w] ends with an org like edu or com
+            /*
+             * Use Regular expression to evaluate the email [\w-_.+]*[\w-_.]@1 or more
+             * characters with -_and . followed by @ ([\w]+\.)+ matches 1 or more domains
+             * like cs.nyu. or pqs.cs.nyu. [\w]+[\w] ends with an org like edu or com
              */
             if (val != null && !val.equals("")) {
                 String regex = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$";
@@ -227,8 +228,8 @@ public class Contact implements Comparable<Contact> {
         }
 
         /**
-         * Build and return the Contact by passing in the builder object created from the methods
-         * above
+         * Build and return the Contact by passing in the builder object created from
+         * the methods above
          *
          * @return Contact object
          */
